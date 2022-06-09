7e8a8ddcf53f97ce836fee3055c0b297e25064c7
Rohan Goel <rohangoel107@gmail.com>
2022-06-09 18:07:55 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TimeTrackerComponent } from './time-tracker/time-tracker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TimeTrackerComponent
   ],
   imports: [
     BrowserModule
